f41132dfa7662dedb418ca6232a971d218c13cc1
Sergiorey3124 <sergiorey3124@gmail.com>
2023-10-17 14:14:06 -0600

    PayproManager

---
+++ src/app.jsx
@@ -0,0 +1,38 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { Container } from "@mui/material";
+import Home from "./pages/Home";
+import SingIn from "./components/SingIn";
+import SignUp from "./components/SignUp";//Cambiar nombre del archivo
+
+const navArrayLinks = [
+    { title: "Home", path: "/" },
+    { title: "Login", path: "/login" },
+    { title: "Register", path: "/register" },
+  ];
+
+export default function app(){
+    return(
+        <>
+        <Navbar navArrayLinks={navArrayLinks}/>
+        <Container sx={{mt: 5}}>
+
+            <Routes>
+                <Route path="/"
+                element={<Home/>}/>
+            </Routes>
+
+            <Routes>
+                <Route path="/login"
+                element={<SingIn/>}/>
+            </Routes>
+
+            <Routes>
+                <Route path="/register"
+                element={<SignUp/>}/>
+            </Routes>
+        
+        </Container>
+        </>
+    )
+}
