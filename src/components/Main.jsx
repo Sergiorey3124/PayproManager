435bbccbe1b4118e5ace57bf8c34434dd25d6750
bryanchinorm <bryanrodriguezmadrigal@hotmail.com>
2023-11-09 15:41:42 -0600

    Pdfsss

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,6 +5,7 @@ import logImage from "./log.png";
 
 function Main() {
     const [pagos, setPagos] = useState([]);
+    const [usuarios, setUsuarios] = useState([]);
 
     // Función para obtener los datos de la tabla de pagos
     const fetchPagos = async () => {
@@ -20,6 +21,25 @@ function Main() {
         }
     };
 
+    // Función para obtener los datos de la tabla de usuarios
+    const fetchUsuarios = async () => {
+        try {
+            const response = await axios.get('https://paypromanager2.000webhostapp.com/listaUsuarios.php');
+            if (response.data && response.data.length > 0) {
+                setUsuarios(response.data);
+            } else {
+                // Manejar el caso de error o datos vacíos
+            }
+        } catch (error) {
+            // Manejar errores de conexión
+        }
+    };
+
+    // Llamar a fetchUsuarios al cargar el componente
+    useEffect(() => {
+        fetchUsuarios();
+    }, []);
+
     // Llamar a fetchPagos al cargar el componente
     useEffect(() => {
         fetchPagos();
@@ -34,52 +54,61 @@ function Main() {
         doc.addImage(imgData, 'JPEG', 10, 10, 40, 40);
 
         // Título y formato
-    doc.setFontSize(16);
-    doc.setFont('helvetica', 'bold');
-    doc.text('Comprobante de Pago', 105, 20, null, null, 'center');
+        doc.setFontSize(16);
+        doc.setFont('helvetica', 'bold');
+        doc.text('Comprobante de Pago', 105, 20, null, null, 'center');
 
-    // Línea horizontal
-    doc.setLineWidth(0.5);
-    doc.line(10, 55, 200, 55);
+        // Línea horizontal
+        doc.setLineWidth(0.5);
+        doc.line(10, 55, 200, 55);
 
-    // Contenido del comprobante
-    doc.setFontSize(12);
-    doc.setFont('helvetica', 'normal');
-    doc.text(`ID del Pago: ${pago.pago_id}`, 15, 70);
-    doc.text(`Fecha del Pago: ${pago.fecha}`, 15, 80);
-    doc.text(`RFC del Cliente: ${pago.RFC}`, 15, 90);
-    doc.text(`Monto Pagado: $${pago.monto}`, 15, 100);
-    doc.text(`Método de Pago: ${pago.metodo_pago}`, 15, 110);
+        // Contenido del comprobante
+        doc.setFontSize(12);
+        doc.setFont('helvetica', 'normal');
+        doc.text(`ID del Pago: ${pago.pago_id}`, 15, 70);
+        doc.text(`Fecha del Pago: ${pago.fecha}`, 15, 80);
+        doc.text(`RFC del Cliente: ${pago.RFC}`, 15, 90);
+        doc.text(`Monto Pagado: $${pago.monto}`, 15, 100);
+        doc.text(`Método de Pago: ${pago.metodo_pago}`, 15, 110);
 
-    // Línea horizontal
-    doc.line(10, 120, 200, 120);
+        // Línea horizontal
+        doc.line(10, 120, 200, 120);
 
-    // Nota
-    doc.setFontSize(10);
-    doc.setFont('helvetica', 'normal');
-    doc.text('Eaqui va lo del rfc.', 15, 130);
+        // Información del cliente (agregada)
+        doc.setFontSize(10);
+        doc.setFont('helvetica', 'normal');
+        doc.text('INFORMACION DEL CLIENTE:', 15, 130);
 
+        // Obtener información del usuario
+        const usuario = usuarios.find((u) => u.id === pago.usuario_id);
+        if (usuario) {
+            doc.text(`Nombre: ${usuario.nombre}`, 15, 140);
+            doc.text(`Correo Electrónico: ${usuario.correo}`, 15, 150);
+            doc.text(`Direccion: ${usuario.direccion}`, 15, 160);
+            doc.text(`NSS: ${usuario.NSS}`, 15, 170);
+            // Puedes agregar más campos según la estructura de tu tabla Usuario
+        } else {
+            doc.text('Información del cliente no disponible', 15, 140);
+        }
 
+        // ... (código existente)
 
+        // Fecha y firma
+        doc.setFontSize(12);
+        doc.setFont('helvetica', 'bold');
+        doc.text('Fecha del Pago:', 140, 150);
+        doc.text('Firma del Cliente:', 140, 170);
 
+        // Línea horizontal
+        doc.line(10, 180, 200, 180);
 
-    // Fecha y firma
-    doc.setFontSize(12);
-    doc.setFont('helvetica', 'bold');
-    doc.text('Fecha del Pago:', 140, 150);
-    doc.text('Firma del Cliente:', 140, 170);
-
-    // Línea horizontal
-    doc.line(10, 180, 200, 180);
-
-    // Pie de página
-    doc.setFontSize(10);
-    doc.setFont('helvetica', 'normal');
-    doc.text('Gracias por su pago. Para más información, por favor contacte con nuestro servicio de atención al cliente.', 15, 195);
-
-    doc.save('comprobante_pago.pdf');
-};
-
+        // Pie de página
+        doc.setFontSize(10);
+        doc.setFont('helvetica', 'normal');
+        doc.text('Gracias por su confianza, Recuerda que PayProManager te permite tener un control total sobre tus finanzas empresariales.', 15, 190);
+        doc.text("Para más información, por favor contacte con nuestro servicio de atención al cliente", 17, 200);
+        doc.save('comprobante_pago.pdf');
+    };
 
     return (
         <div className="container mt-4">
