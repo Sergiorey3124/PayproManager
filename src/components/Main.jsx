b2a69c728d85c4346470ae5a292f0af84719b1fa
bryanchinorm <bryanrodriguezmadrigal@hotmail.com>
2023-11-27 19:43:39 -0600

    Confir pago

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,19 +1,80 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import jsPDF from 'jspdf';
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
 import logImage from "./log.png";
 
 function Main() {
     const [pagos, setPagos] = useState([]);
     const [usuarios, setUsuarios] = useState([]);
-    const [showConfirmModal, setShowConfirmModal] = useState(false);
-    const [selectedPago, setSelectedPago] = useState(null);
-    const [confirmDate, setConfirmDate] = useState("");
 
-    const handleCloseConfirmModal = () => setShowConfirmModal(false);
-    const handleShowConfirmModal = () => setShowConfirmModal(true);
+    const [showModal, setShowModal] = useState(false);
+    const [selectedPayment, setSelectedPayment] = useState(null);
+    const [selectedDate, setSelectedDate] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    
+        if (!selectedPayment) {
+            console.error('Error: selectedPayment no está configurado correctamente.');
+            return;
+        }
+    
+        // Formatear la fecha al formato "año-mes-día"
+        const formattedDate = new Date(selectedDate).toISOString().split("T")[0];
+    
+        // Asegúrate de que selectedPayment contiene la información necesaria
+        const pago_id = selectedPayment.pago_id;
+    
+        // Modificar la lógica de estado según sea necesario
+        const estado = 1; // Cambia esto según tus necesidades (1 para true, 0 para false)
+    
+        axios.post('https://paypromanager2.000webhostapp.com/php/editpagos.php', {
+            pago_id: pago_id,
+            fecha: formattedDate,
+            estado: estado
+        })
+        .then((respuesta) => {
+            if (respuesta.data.success) {
+                // Actualizar el estado del pago en la tabla
+                setPagos(pagos.map((p) => {
+                    if (p.pago_id === pago_id) {
+                        p.fecha = formattedDate;
+                        p.estado = estado;
+                    }
+                    return p;
+                }));
+    
+                // Cerrar la ventana emergente
+                closeModal();
+    
+                // Mostrar un mensaje de confirmación
+                alert('Pago confirmado correctamente');
+            } else {
+                // Mostrar un mensaje de error
+                alert('Error al confirmar el pago');
+            }
+        })
+        .catch((error) => {
+            // Manejar errores de conexión
+            console.error('Error al confirmar el pago:', error);
+        });
+    };
+    
+    
+    
+    
+    const openModal = (pago) => {
+        setShowModal(true);
+        setSelectedPayment(pago);
+        setSelectedDate(pago.fecha);
+    };
+
+    // Función para cerrar la ventana emergente
+    const closeModal = () => {
+        setShowModal(false);
+        setSelectedPayment(null);
+        setSelectedDate('');
+    };
 
     const fetchPagos = async () => {
         try {
@@ -25,6 +86,7 @@ function Main() {
             }
         } catch (error) {
             // Manejar errores de conexión
+            console.error('Error al obtener pagos:', error);
         }
     };
 
@@ -38,33 +100,105 @@ function Main() {
             }
         } catch (error) {
             // Manejar errores de conexión
+            console.error('Error al obtener usuarios:', error);
         }
     };
 
-    const Confirmar = (pago) => {
-        setSelectedPago(pago);
-        handleShowConfirmModal();
-    };
-
-    const handleDateChange = (event) => {
-        setConfirmDate(event.target.value);
-    };
-
-    const handleConfirmAction = () => {
-        // Validar que se haya ingresado una fecha
-        if (!confirmDate) {
-            // Puedes mostrar un mensaje de error o tomar la acción que prefieras
-            alert("Por favor, ingrese una fecha de confirmación.");
-            return;
-        }
-
-        // Puedes acceder a confirmDate para realizar la acción que necesites
-        console.log("Fecha de confirmación:", confirmDate);
-
-        // Aquí puedes realizar la lógica adicional antes de confirmar el pago
-        generatePDF(selectedPago);
-        handleCloseConfirmModal();
-    };
+    function Confirmar(pago) {
+        const fechaActual = new Date();
+        const fechaFormateada = fechaActual.toISOString().split("T")[0];
+    
+        // Crear una nueva ventana emergente
+        const modal = window.open("", "Confirmar Pago", "width=500,height=300");
+    
+        // Estilos CSS para mejorar la apariencia
+        modal.document.head.innerHTML = `
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    background-color: #f4f4f4;
+                    margin: 0;
+                    padding: 20px;
+                    text-align: center;
+                }
+                .container {
+                    background-color: #fff;
+                    border-radius: 5px;
+                    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                    padding: 20px;
+                    max-width: 400px;
+                    margin: 0 auto;
+                }
+                h1 {
+                    color: #333;
+                }
+                label {
+                    display: block;
+                    margin-top: 10px;
+                    font-weight: bold;
+                }
+                input {
+                    width: 100%;
+                    padding: 8px;
+                    margin-top: 5px;
+                    margin-bottom: 15px;
+                    box-sizing: border-box;
+                }
+                input[type="submit"] {
+                    background-color: #4caf50;
+                    color: white;
+                    border: none;
+                    padding: 10px;
+                    border-radius: 4px;
+                    cursor: pointer;
+                }
+            </style>
+        `;
+    
+        // Contenido del formulario
+        modal.document.body.innerHTML = `
+            <div class="container">
+                <h1>Confirmar Pago</h1>
+                <form action="https://paypromanager2.000webhostapp.com/php/editpagos.php" method="post">
+                    <input type="hidden" name="pago_id" value=${pago.pago_id} />
+                    <input type="hidden" name="estado" value="1" />
+                    <label for="fecha">Fecha</label>
+                    <input type="date" name="fecha" id="fecha" value=${fechaFormateada} required />
+                    <br>
+                    <input type="submit" value="Confirmar Pago">
+                </form>
+            </div>
+        `;
+    
+        // Enviar la solicitud POST al servidor
+        axios.post('https://paypromanager2.000webhostapp.com/php/editpagos.php', {
+            pago_id: pago.pago_id,
+            fecha: fechaFormateada,
+            estado: 1
+        })
+        .then((respuesta) => {
+            if (respuesta.data.success) {
+                // Actualizar el estado del pago en la base de datos
+                setPagos(pagos.map((p) => {
+                    if (p.pago_id === pago.pago_id) {
+                        p.estado = true;
+                    }
+                    return p;
+                }));
+    
+                // Mostrar un mensaje de confirmación
+                alert('Pago confirmado correctamente');
+            } else {
+                // Mostrar un mensaje de error
+                alert('Error al confirmar el pago');
+            }
+        })
+        .catch((error) => {
+            // Manejar errores de conexión
+            console.error('Error al confirmar el pago:', error);
+        });
+    }
+    
 
     const generatePDF = (pago) => {
         const doc = new jsPDF();
@@ -130,82 +264,71 @@ function Main() {
 
     useEffect(() => {
         fetchUsuarios();
-    }, []);
-
-    useEffect(() => {
         fetchPagos();
     }, []);
 
     return (
-        <div className="container mt-4">
-            <table className="table table-bordered">
-                <thead className="thead-dark">
-                    <tr>
-                        <th>ID</th>
-                        <th>RFC</th>
-                        <th>Monto</th>
-                        <th>Fecha</th>
-                        <th>Fecha de Aviso</th>
-                        <th>Método de Pago</th>
-                        <th>Estado</th>
-                        <th>Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {pagos.map((pago) => (
-                        <tr key={pago.pago_id}>
-                            <td>{pago.pago_id}</td>
-                            <td>{pago.RFC}</td>
-                            <td>{pago.monto}</td>
-                            <td>{pago.fecha}</td>
-                            <td>{pago.fecha_aviso}</td>
-                            <td>{pago.metodo_pago}</td>
-                            <td>{pago.estado ? 'Pagado' : 'En Espera'}</td>
-                            <td>
-                                {pago.estado === 1 ? (
-                                    <button className="btn btn-primary" onClick={() => generatePDF(pago)}>
-                                        Imprimir PDF
-                                    </button>
-                                ) : (
-                                    <button className="btn btn-warning" onClick={() => Confirmar(pago)}>
-                                        Confirmar Pago
-                                    </button>
-                                )}
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-
-            <Modal show={showConfirmModal} onHide={handleCloseConfirmModal} centered>
-                <Modal.Header closeButton>
-                    <Modal.Title>Confirmar Pago</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <p>¿Estás seguro de que deseas confirmar el pago?</p>
-                    <form>
-                        <div className="form-group">
-                            <label htmlFor="confirmDate">Fecha de Confirmación:</label>
+        <div>
+            {showModal && (
+                <div className="modal">
+                    <div className="modal-content">
+                        <span className="close" onClick={closeModal}>&times;</span>
+                        <h1>Actualizar Pago</h1>
+                        <form onSubmit={(e) => handleSubmit(e)}>
+                            <label htmlFor="fecha">Fecha:</label>
                             <input
                                 type="date"
-                                id="confirmDate"
-                                value={confirmDate}
-                                onChange={handleDateChange}
-                                className="form-control"
-                                required
+                                id="fecha"
+                                value={selectedDate}
+                                onChange={(e) => setSelectedDate(e.target.value)}
                             />
-                        </div>
-                    </form>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleCloseConfirmModal}>
-                        Cancelar
-                    </Button>
-                    <Button variant="primary" onClick={handleConfirmAction}>
-                        Confirmar
-                    </Button>
-                </Modal.Footer>
-            </Modal>
+                            <br />
+                            <button type="submit">Confirmar Pago</button>
+                        </form>
+                    </div>
+                </div>
+            )}
+
+            <div className="container mt-4">
+                <table className="table table-bordered">
+                    <thead className="thead-dark">
+                        <tr>
+                            <th>ID</th>
+                            <th>RFC</th>
+                            <th>Monto</th>
+                            <th>Fecha</th>
+                            <th>Fecha de Aviso</th>
+                            <th>Método de Pago</th>
+                            <th>Estado</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {pagos.map((pago) => (
+                            <tr key={pago.pago_id}>
+                                <td>{pago.pago_id}</td>
+                                <td>{pago.RFC}</td>
+                                <td>{pago.monto}</td>
+                                <td>{pago.fecha}</td>
+                                <td>{pago.fecha_aviso}</td>
+                                <td>{pago.metodo_pago}</td>
+                                <td>{pago.estado ? 'Pagado' : 'En Espera'}</td>
+                                <td>
+                                    {pago.estado === 1 ? (
+                                        <button className="btn btn-primary" onClick={() => generatePDF(pago)}>
+                                            Imprimir PDF
+                                        </button>
+                                    ) : (
+                                        <button className="btn btn-warning" onClick={() => Confirmar(pago)}>
+                                            Confirmar Pago
+                                        </button>
+                                    )}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 }
